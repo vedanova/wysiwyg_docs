1e974a226ba123a168d25858a9b7a06cdb88fefd
Christoph Klocker <christoph@vedanova.com>
2012-12-13 12:18:50 +0100

    implemented flash notice, fixed empty doc bug, allow configuration of sidebar root title

--- app/controllers/wysiwyg_docs/docs_controller.rb
+++ app/controllers/wysiwyg_docs/docs_controller.rb
@@ -4,14 +4,21 @@ module WysiwygDocs
   class DocsController < ApplicationController
 
     def index
-      @doc = Doc.first
+      @doc = Doc.first || Doc.new
       @topics = Topic.all
+    end
+
+    def create
+      @doc = Doc.new(params[:doc])
+      if @doc.save!
+        redirect_to docs_path, :notice => "Successfully updated"
+      end
     end
 
     def update
       @doc = Doc.find(params[:id])
       if @doc.update_attributes(params[:doc])
-        redirect_to docs_path
+        redirect_to docs_path, :notice => "Successfully updated"
       end
     end
 
